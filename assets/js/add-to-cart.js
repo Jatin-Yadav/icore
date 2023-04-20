c5204d0f76e2759078850bbe064c7710fa8291a6
jatin-yadav <jatinyadav19oct@gmail.com>
2023-04-20 18:04:55 +0530

    icore add to cart startup

---
+++ assets/js/add-to-cart.js
@@ -0,0 +1,109 @@
+let productsInCart = JSON.parse(localStorage.getItem("shoppingCart"));
+if (!productsInCart) {
+  productsInCart = [];
+}
+const cartElements = document.querySelector(".minicart-items");
+const noCartElements = document.querySelector(".zentimo-submenu");
+const cartCount = document.querySelector(".cart-count");
+const cartAmount = document.querySelector(".Price-amount");
+const products = document.querySelectorAll(".product-inner");
+
+const countTheSumPrice = function () {
+  let sum = 0;
+  productsInCart.forEach((item) => {
+    sum += item.price;
+  });
+  return sum;
+};
+const updateShoppingCartHTML = function () {
+  localStorage.setItem("shoppingCart", JSON.stringify(productsInCart));
+  if (productsInCart.length > 0) {
+    let result = productsInCart.map((product) => {
+      return `
+      <li class="product-cart mini_cart_item">
+                              <a href="#" class="product-media">
+                                <img
+                                  src="${product.product_image}"
+                                  alt="img"
+                                />
+                              </a>
+                              <div class="product-details">
+                                <h5 class="product-name">
+                                  <a href="#">${product.name}</a>
+                                </h5>
+                                <div class="variations">
+                                  <span class="attribute_color">
+                                    <a href="#">${product.attribute_color}</a>
+                                  </span>
+                                  ,
+                                  <span class="attribute_size">
+                                    <a href="#">${product.attribute_size}</a>
+                                  </span>
+                                </div>
+                                <span class="product-price">
+                                  <span class="price">
+                                    <span>₹${product.basePrice}</span>
+                                  </span>
+                                </span>
+                                <span class="product-quantity"> (x${product.quantity}) </span>
+                                <div class="product-remove">
+                                  <a href="#"
+                                    ><i class="fa fa-trash-o" aria-hidden="true"></i
+                                  ></a>
+                                </div>
+                              </div>
+                            </li>
+        `;
+    });
+
+    cartElements.innerHTML = result.join("");
+    cartCount.innerHTML = productsInCart.length;
+    cartAmount.innerHTML = `₹${countTheSumPrice()}`;
+    document.querySelector(".no-product").classList.add("hidden");
+    document.querySelector(".shopcart-description").classList.remove("hidden");
+  } else {
+    document.querySelector(".shopcart-description").classList.add("hidden");
+    document.querySelector(".no-product").classList.remove("hidden");
+  }
+};
+
+function updateProductsInCart(product) {
+  for (let i = 0; i < productsInCart.length; i++) {
+    if (productsInCart[i].id == product.id) {
+      productsInCart[i].quantity += 1;
+      productsInCart[i].price =
+        productsInCart[i].basePrice * productsInCart[i].quantity;
+      return;
+    }
+  }
+  productsInCart.push(product);
+}
+
+products.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (e.target.classList.contains("single_add_to_cart_button")) {
+      const productID = e.target.dataset.productId;
+      const productName = item
+        .querySelector(".product-name")
+        .querySelector("a").innerHTML;
+      const productPrice = item
+        .querySelector(".price")
+        .querySelector("ins").innerHTML;
+      const productImage = item.querySelector("img").src;
+      let product = {
+        id: productID,
+        name: productName,
+        attribute_color: "Black",
+        attribute_size: "",
+        price: Number(productPrice.replace("$", "")),
+        basePrice: Number(productPrice.replace("$", "")),
+        quantity: 1,
+        product_image: productImage,
+      };
+      updateProductsInCart(product);
+      updateShoppingCartHTML();
+    }
+  });
+});
+
+updateShoppingCartHTML();
